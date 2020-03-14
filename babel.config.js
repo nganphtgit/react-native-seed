8f9f16bbcb50fc2726fee243dce9686634123a07
Ngân <tuyetnganphamhoang@gmail.com>
2020-03-14 19:27:14 +0700

    fix resolve absolute path - add bracket []

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,11 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
-      'module-resolver', {
-        root: ['./src']
-      }
+      ["module-resolver", {
+        root: ["./src"],
+      }],
     ],
   };
 };
